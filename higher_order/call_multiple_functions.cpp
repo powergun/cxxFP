661eae45ba245eab9ff6428366cbe8e5870b4273
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-05-08 17:00:09 +1000

    higher order function: variadic function call

---
+++ higher_order/call_multiple_functions.cpp
@@ -0,0 +1,12 @@
+//
+// Created by weining on 8/5/20.
+//
+
+// c++ stl cookbook P/155
+// this recipe also shows how to implement a framework that apply a function
+// to any number of arguments (variadic) even the standard does not natively
+// support the fold expression on it
+
+// auto apply_multi(Ts ...args, Fs ...functions) {
+//
+// }
