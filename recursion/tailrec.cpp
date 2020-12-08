7e9d05cd705b57ff3b197a773c723083ccc6076b
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-12-08 14:35:00 +1100

    FP in c++

--- recursion/tailrec.cpp
+++ recursion/tailrec.cpp
@@ -9,7 +9,11 @@
 #include "../vendor/auto_timer.h"
 
 // experiment with tailrec
-// at the time of writing (gcc-9, c++17) tail call is NOT optimized
+// at the time of writing (gcc-9, c++17) tail call is NOT optimized BY DEFAULT;
+// but it can enabled via a compiler flag:
+// https://stackoverflow.com/questions/490324/how-do-i-check-if-gcc-is-performing-tail-recursion-optimization
+// https://www.reddit.com/r/C_Programming/comments/4hazah/gcc_tailcall_recursion_optimization/
+// https://dev.to/rohit/demystifying-tail-call-optimization-5bf3
 
 int tailrecSum( const std::vector< int > &xs, size_t idx, int acc )
 {
